a17d6650374a927ed66d562e3fd9849a4ae542e5
Sausville <SausSam18@issaquah.wednet.edu>
2017-09-25 10:03:52 -0700

    just a little work needed

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        List<String> list = new ArrayList<String>();
+        Set<String> set = new HashSet<String>(list);
+        File file = new File("hamlet.txt");
+        FileInputStream f = new FileInputStream(file);
+        Scanner scan = new Scanner(f);
+        int countTotal = 0;
+        while(scan.hasNext()){
+            String check = scan.next();
+            if(set.contains(check) == false){
+                set.add(check);
+            }
+            countTotal++;
+        }
+        int countNew = set.size();
+        System.out.println("This is Total word count " + countTotal);
+        System.out.println("This is Unique word count " + countNew);
+
+    }
+}
